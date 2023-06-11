a847267dec00d5e05d607f6a18fd72d02e08a7ec
shawn Wong <jadesure17@gmail.com>
2023-06-11 12:39:40 +1000

    consider about adding validation

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,11 @@
 import { TaskStatus } from '../task.model';
+import { IsEnum, IsOptional } from 'class-validator';
 
 export class GetTasksFilterDto {
+  @IsEnum(TaskStatus)
+  @IsOptional()
   status?: TaskStatus;
+
+  @IsOptional()
   search?: string;
 }
